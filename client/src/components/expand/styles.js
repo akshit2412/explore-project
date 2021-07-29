cf8afb260c3afd399a9669cebc01d9fe4be8bdec
Your Name <you@example.com>
2021-07-29 23:55:46 +0530

    styles changed

--- client/src/components/expand/styles.js
+++ client/src/components/expand/styles.js
@@ -1,37 +1,70 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-    heading: {
-        fontSize: '3rem',
-        margin: '0% 15%',
-        fontFamily: 'fell, Georgia, Cambria, "Times New Roman", Times, serif',
-    },
-    info: {
-        fontSize: 15,
-        margin: '1% 15% 2% 15%',
 
-    },
-    post: {
-        fontFamily: "Cambria, Times New Roman, Times, serif",
-        margin: '2% 15% 2%',
-        fontSize: 22,
-        whiteSpace: 'pre-wrap',
-        wordSpacing: '3px',
-    },
-    expand: {
-        padding: '1.5% 9%',
-
-    },
-    card: {
-        width: '50%',
-        height: '50vh',
-        margin: '0% 15%',
-    },
     media: {
         height: '100%',
         width: '100%',
         objectFit: 'contain',
     },
+    [theme.breakpoints.down("sm")]: {
+        expand: {
+            padding: '1.5% 5%',
+        },
+        card: {
+            width: '70%',
+            height: '30vh',
+            margin: '2% 10%',
+        },
+
+        heading: {
+            fontSize: '2rem',
+            margin: '0% 10%',
+            fontFamily: 'fell, Georgia, Cambria, "Times New Roman", Times, serif',
+        },
+        info: {
+            fontSize: 15,
+            margin: '1% 15% 5% 10%',
+
+        },
+        post: {
+            fontFamily: "Cambria, Times New Roman, Times, serif",
+            margin: '5% 15% 2% 10%',
+            fontSize: 10,
+            whiteSpace: 'pre-wrap',
+            wordSpacing: '3px',
+        },
+
+    },
+    [theme.breakpoints.up("sm")]: {
+        expand: {
+            padding: '1.5% 9%',
+        },
+        card: {
+            width: '50%',
+            height: '50vh',
+            margin: '0% 15%',
+        },
+        heading: {
+            fontSize: '3rem',
+            margin: '0% 15%',
+            fontFamily: 'fell, Georgia, Cambria, "Times New Roman", Times, serif',
+        },
+        info: {
+            fontSize: 15,
+            margin: '1% 15% 2% 15%',
+
+        },
+        post: {
+            fontFamily: "Cambria, Times New Roman, Times, serif",
+            margin: '2% 15% 2%',
+            fontSize: 22,
+            whiteSpace: 'pre-wrap',
+            wordSpacing: '3px',
+        },
+
+    },
+
     close: {
         position: 'sticky',
         top: 0,
@@ -56,13 +89,7 @@ export default makeStyles((theme) => ({
         marginRight: '1%',
 
     }
-    // likerequest: {
-    //     marginLeft: '1%',
-    //     fontFamily: "fell, Georgia, Cambria, Times New Roman, Times, serif",
-    //     fontSize: 18,
-    //     color: 'blue',
-    //     fontWeight: 'bold',
-    // },
+
 
 
 
